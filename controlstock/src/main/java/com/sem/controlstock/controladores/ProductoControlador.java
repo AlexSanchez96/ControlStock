5bb92b2f488ddb43cd353fe84fa5ace37d0923e6
AlexSanchez96 <alexisgsanchez09@gmail.com>
2022-11-24 18:40:40 -0300

    Segundo Commit

--- controlstock/src/main/java/com/sem/controlstock/controladores/ProductoControlador.java
+++ controlstock/src/main/java/com/sem/controlstock/controladores/ProductoControlador.java
@@ -1,12 +1,13 @@
 package com.sem.controlstock.controladores;
 
+import com.sem.controlstock.entidades.Proveedor;
 import com.sem.controlstock.excepciones.MiException;
 import com.sem.controlstock.servicios.ProductoServicio;
 import com.sem.controlstock.servicios.ProveedorServicio;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,10 @@ public class ProductoControlador {
     private ProductoServicio productoServicio;
     
     @GetMapping("/registrar")
-    public String registrar(){
+    public String registrar(ModelMap modelo){
+        List<Proveedor> proveedores = proveedorServicio.listarProveedores();
+        
+        modelo.addAttribute("proveedores", proveedores);
         
         return "producto_form.html";
         
@@ -32,12 +36,19 @@ public class ProductoControlador {
     @PostMapping("/registro")
     public String registro(@RequestParam String id, @RequestParam String nombre,
             @RequestParam(required = false) Float cantidad, @RequestParam String descripcion,
-            @RequestParam Float precio, @RequestParam String idProveedor){
+            @RequestParam(required = false) Float precio, @RequestParam String idProveedor,
+            ModelMap modelo){
         
         try {
             productoServicio.crearProducto(id, nombre, cantidad, descripcion, precio, idProveedor);
+            
+            modelo.put("exito", "el producto fue cargado correctamente");
         } catch (MiException ex) {
-            Logger.getLogger(ProductoControlador.class.getName()).log(Level.SEVERE, null, ex);
+            
+            List<Proveedor> proveedores = proveedorServicio.listarProveedores();
+            modelo.addAttribute("proveedores", proveedores);
+            
+            modelo.put("error", ex.getMessage());
             return "producto_form.html";
         }
         return "index.html";
--- controlstock/src/main/java/com/sem/controlstock/controladores/ProveedorControlador.java
+++ controlstock/src/main/java/com/sem/controlstock/controladores/ProveedorControlador.java
@@ -1,11 +1,14 @@
 package com.sem.controlstock.controladores;
 
+import com.sem.controlstock.entidades.Proveedor;
 import com.sem.controlstock.excepciones.MiException;
 import com.sem.controlstock.servicios.ProveedorServicio;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ProveedorControlador {
     
     @Autowired
-    private ProveedorServicio proveedorservicio;
+    private ProveedorServicio proveedorServicio;
     
     @GetMapping("/registrar")
     public String registrar(){
@@ -24,16 +27,31 @@ public class ProveedorControlador {
     }
     
     @PostMapping("/registro")
-    public String registro(@RequestParam String nombre, @RequestParam String email, @RequestParam String lugar, @RequestParam String telefono){
+    public String registro(@RequestParam String nombre, @RequestParam String email, @RequestParam String lugar,
+            @RequestParam String telefono, ModelMap modelo){
         
         try {
-            proveedorservicio.crearProveedor(nombre, email, lugar, telefono);
+            proveedorServicio.crearProveedor(nombre, email, lugar, telefono);
+            modelo.put("exito", "El proveedor fue cargado correctamente");
         } catch (MiException ex) {
-            Logger.getLogger(ProveedorControlador.class.getName()).log(Level.SEVERE, null, ex);
+            
+            modelo.put("error", ex.getMessage());
             return "proveedor_form.html";
         }
         
         return "index.html";
     }
+    
+    @GetMapping("/lista")
+    public String listar(ModelMap modelo){
+        
+        List<Proveedor> proveedores = proveedorServicio.listarProveedores();
+        
+        modelo.addAttribute("proveedores", proveedores);
+        
+        return "proveedor_list.html";
+        
+    }
+    
     
 }
